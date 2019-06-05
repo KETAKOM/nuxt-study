5904e766f2a371cf69273168f3eb96ccf1da2891
kotake_shunki <ketakom2111178@gmail.com>
2019-06-06 00:32:33 +0900

    add sample

---
+++ docker/nuxt/src/app/pages/sample/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <section class="container">
+    <div>
+      <h1 class="title">
+        サンプルページです。
+      </h1>
+      
+      <nuxt-link to="/">トップページへのリンク</nuxt-link>
+    </div>
+  </section>
+</template>
