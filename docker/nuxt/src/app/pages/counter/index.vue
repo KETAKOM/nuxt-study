d4ac8aa2105a625eccd51e309db694cc0284e343
kotake_shunki <ketakom2111178@gmail.com>
2019-06-06 01:47:06 +0900

    add counter

---
+++ docker/nuxt/src/app/pages/counter/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <section class="container">
+    <div>
+      <h1>買い物かご</h1>
+      <p>リンゴの数:{{apple}}個</p>
+      <p>みかんの数:{{orange}}個</p>
+
+      <button @click="addApple">リンゴ+1</button>
+      <button @click="removeApple">リンゴ-1</button>
+      <button @click="addOrange">みかん+1</button>
+      <button @click="removeOrange">みかん-1</button>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  computed: {
+    apple () { return this.$store.state.counter.apple },
+    orange () {return this.$store.state.counter.orange}
+  },
+  methods: {
+    addApple (e) {
+      this.$store.commit('counter/addApple')
+    },
+    removeApple (e) {
+      this.$store.commit('counter/removeApple')
+    },
+    addOrange () {
+        this.$store.commit('counter/addOrange')
+    },
+    removeOrange () {
+        this.$store.commit('counter/removeOrange')
+    }
+  }
+}
+</script>
--- docker/nuxt/src/app/store/counter.js
+++ docker/nuxt/src/app/store/counter.js
@@ -1,9 +1,19 @@
 export const state = () => ({
-    count: 0
-  })
+    apple: 0,
+    orange: 0
+})
   
-  export const mutations = {
-    add (state) {
-      state.count += 1
+export const mutations = {
+    addApple (state) {
+        state.apple += 1
+    },
+    removeApple (state) {
+        state.apple -= 1
+    },
+    addOrange (state) {
+        state.orange += 1
+    },
+    removeOrange (state) {
+        state.apple -= 1
     }
-  }
+}
