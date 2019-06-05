80388cdcee6076f2c0fe10d65ab2035b6fde7b6f
kotake_shunki <ketakom2111178@gmail.com>
2019-06-06 00:47:19 +0900

    add AppSample.vue components

---
+++ docker/nuxt/src/app/components/AppSample.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="sample">
+    <p>サンプルコンポーネントです</p>
+  </div>
+</template>
+
+<style>
+.sample {
+  display: inline-block;
+  animation: turn 2s linear forwards 1s;
+  transform: rotateX(180deg);
+  position: relative;
+  overflow: hidden;
+  height: 180px;
+  width: 245px;
+}
+</style>
+
--- docker/nuxt/src/app/pages/sample/index.vue
+++ docker/nuxt/src/app/pages/sample/index.vue
@@ -4,8 +4,20 @@
       <h1 class="title">
         サンプルページです。
       </h1>
+      <app-sample/>
       
       <nuxt-link to="/">トップページへのリンク</nuxt-link>
+      
     </div>
   </section>
 </template>
+
+<script>
+import AppSample from '~/components/AppSample.vue'
+
+export default {
+  components: {
+    AppSample
+  }
+}
+</script>
